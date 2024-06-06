f91a7814d9ae30b1c8b2b17b6777489cce50b627
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-06-06 10:00:01 -0400

    Add dynamic navbar menu to main app
    This commit introduces a dynamic navbar menu to the main application. The changes include the modification of the Navbar structure, swapping hardcoded NavbarItem elements with a generated list of items. It also integrates page navigation logic directly in the NavbarMenu and NavbarMenuToggle, improving usability for all screen sizes.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import $ from 'jquery'
-import {Link, NextUIProvider} from "@nextui-org/react";
+import {Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, NextUIProvider} from "@nextui-org/react";
 
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
 
@@ -39,27 +39,44 @@ function PageContent() {
 
 
 function Nav() {
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const pages = {
+        "Home": "/",
+        "About": "/about",
+    };
+    const menuItems = Object.keys(pages).map((item, index) => {
+        const url = Object.values(pages)[index];
+        const isCurrentPage = window.location.pathname === url;
+        return (
+            <NavbarMenuItem>
+                <Link href={url} color={isCurrentPage ? "primary" : "foreground"} aria-current="page" size="lg" className="w-full">
+                    {item}
+                </Link>
+            </NavbarMenuItem>
+        );
+    });
+
+
     return (
-        <Navbar>
-            <NavbarBrand>
-                <p className="font-bold text-inherit">Example</p>
-            </NavbarBrand>
+        <Navbar onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">Example</p>
+                </NavbarBrand>
+            </NavbarContent>
+
             <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem>
-                    <Link color="foreground" href="/">
-                        Home
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="/about">
-                        About
-                    </Link>
-                </NavbarItem>
+                {menuItems}
             </NavbarContent>
             <NavbarContent justify="end">
                 <NavbarItem>
                     <ThemeSwitcher/>
                 </NavbarItem>
             </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems}
+            </NavbarMenu>
         </Navbar>)
 }
