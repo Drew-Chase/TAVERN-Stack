67a9af06e7b23e32105cfab125595444c9afeb27
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-14 15:53:45 -0400

    Refactor ThemeSwitcher and add Navigation component
    Refactored ThemeSwitcher to use FontAwesome icons and updated theme management logic. Added a new Navigation component with responsive menu items and integrated ThemeSwitcher.

---
+++ src/assets/components/Navigation.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle} from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
+import ThemeSwitcher from "./ThemeSwitcher.tsx";
+
+export default function Navigation()
+{
+
+    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+    const pages = {
+        "Home": "/",
+        "About": "/about"
+    };
+    const menuItems = Object.keys(pages).map((item, index) =>
+    {
+        const url = Object.values(pages)[index];
+        const isCurrentPage = window.location.pathname === url;
+        return (
+            <NavbarMenuItem key={`${item}-${index}`}>
+                <Link href={url} color={isCurrentPage ? "primary" : "foreground"} aria-current="page" size="lg" className="w-full">
+                    {item}
+                </Link>
+            </NavbarMenuItem>
+        );
+    });
+
+
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent>
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
+                <NavbarBrand>
+                    <p className="font-bold text-inherit">Example</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                {menuItems}
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem>
+                    <ThemeSwitcher/>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems}
+            </NavbarMenu>
+        </Navbar>);
+}
