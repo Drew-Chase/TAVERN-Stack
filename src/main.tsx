c4276c36a9dc1727bd427387add615c6d8649a4d
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-14 15:53:34 -0400

    Refactor main component and navigation setup
    Renamed and streamlined main renderer component to `MainContentRenderer`. Consolidated and refactored navigation-related imports and logic into a single `Navigation` component, removing redundant code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,31 @@
-import React from 'react'
-import {BrowserRouter, Route, Routes, useNavigate} from 'react-router-dom';
-import ReactDOM from 'react-dom/client'
-import $ from 'jquery'
-import {Link, NavbarMenu, NavbarMenuItem, NavbarMenuToggle, NextUIProvider} from "@nextui-org/react";
+import React from "react";
+import {BrowserRouter, Route, Routes, useNavigate} from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import $ from "jquery";
+import {NextUIProvider} from "@nextui-org/react";
 
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/navbar";
-
-import "./assets/scss/index.scss"
+import "./assets/scss/index.scss";
 import Home from "./assets/pages/Home.tsx";
 import About from "./assets/pages/About.tsx";
-import ThemeSwitcher, {applyTheme} from "./assets/components/ThemeSwitcher.tsx";
+import Navigation from "./assets/components/Navigation.tsx";
+import {applyTheme} from "./assets/ts/Theme.ts";
 
-applyTheme()
 
 ReactDOM.createRoot($("#root")[0]!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <PageContent/>
+            <MainContentRenderer/>
         </BrowserRouter>
-    </React.StrictMode>,
-)
+    </React.StrictMode>
+);
 
-
-function PageContent() {
+export function MainContentRenderer()
+{
+    applyTheme();
     const navigate = useNavigate();
     return (
         <NextUIProvider navigate={navigate}>
-            <Nav/>
+            <Navigation/>
             <Routes>
                 <Route>
                     <Route path="/" element={<Home/>}/>
@@ -35,48 +34,4 @@ function PageContent() {
             </Routes>
         </NextUIProvider>
     );
-}
-
-
-function Nav() {
-    const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-    const pages = {
-        "Home": "/",
-        "About": "/about",
-    };
-    const menuItems = Object.keys(pages).map((item, index) => {
-        const url = Object.values(pages)[index];
-        const isCurrentPage = window.location.pathname === url;
-        return (
-            <NavbarMenuItem>
-                <Link href={url} color={isCurrentPage ? "primary" : "foreground"} aria-current="page" size="lg" className="w-full">
-                    {item}
-                </Link>
-            </NavbarMenuItem>
-        );
-    });
-
-
-    return (
-        <Navbar onMenuOpenChange={setIsMenuOpen}>
-            <NavbarContent>
-                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"} className="sm:hidden"/>
-                <NavbarBrand>
-                    <p className="font-bold text-inherit">Example</p>
-                </NavbarBrand>
-            </NavbarContent>
-
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                {menuItems}
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem>
-                    <ThemeSwitcher/>
-                </NavbarItem>
-            </NavbarContent>
-
-            <NavbarMenu>
-                {menuItems}
-            </NavbarMenu>
-        </Navbar>)
 }
