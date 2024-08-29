622dc3ac9672e4f26708970eb0cb889801deccf9
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-08-29 10:38:56 -0400

    Remove unused 'cleanup' import in run.js
    The 'cleanup' function import was removed as it was not being used in the script. Added an eslint directive to ignore the undefined variable warning for process.cwd() usage in the execSync command.

--- templates/rust-axium/scripts/run.js
+++ templates/rust-axium/scripts/run.js
@@ -1,4 +1,4 @@
-import {buildBackend, buildFrontend, cleanup} from "./app.js";
+import {buildBackend, buildFrontend} from "./app.js";
 import {execSync} from "node:child_process";
 
 run()
@@ -6,5 +6,6 @@ run()
 function run() {
     buildFrontend();
     buildBackend();
+    // eslint-disable-next-line no-undef
     execSync(`${process.cwd()}/dist/stacked.exe`, {cwd: `${process.cwd()}/dist`, stdio: "inherit"});
 }
