307996213fd6049e871096a8f1db46b47f3ec779
Drew (Linux) <dcmanproductions@gmail.com>
2024-09-25 19:52:38 -0400

    Add build script for Rust Actix project
    This commit introduces a new build script to run the frontend build process using `npm`. It ensures that the frontend assets are built correctly before proceeding with further steps.

--- templates/rust-actix/src-actix/build.rs
+++ templates/rust-actix/src-actix/build.rs
@@ -1,23 +1,16 @@
-use npm_rs::{NodeEnv, NpmEnv};
-use std::fs;
-use walkdir::WalkDir;
+use std::{env};
+use std::process::Command;
 
 fn main() {
-	for entry in WalkDir::new("src") {
-		let entry = entry.unwrap();
-		if entry.file_type().is_file() {
-			println!("cargo:rerun-if-changed={}", entry.path().display());
-		}
-	}
-	fs::create_dir_all("target/dev-env").expect("failed to create target directory");
-	fs::create_dir_all("target/wwwroot").expect("failed to create wwwroot directory");
-	println!("Building frontend...");
 	// Run `npm run build frontend`
-	NpmEnv::default()
-		.with_node_env(&NodeEnv::Production)
-		.init_env()
-		.run("build-frontend")
-		.install(None)
-		.exec()
-		.expect("Failed to execute npm run 'build frontend'");
+	let status = Command::new("npm")
+		.arg("run")
+		.arg("build frontend")
+		.current_dir(env::current_dir().unwrap())
+		.status()
+		.expect("Failed to execute npm 'build frontend'");
+
+	if !status.success() {
+		panic!("npm 'build frontend' failed!");
+	}
 }
