118920bf8c2e76b0b2d4fd3fa2256ffa069413db
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-10-14 14:19:07 -0400

    Refactor build script to use npm_rs and enhance directory setup
    Replaced usage of std::process::Command with npm_rs for cleaner npm script execution. Added logic for creating required directories and monitoring source file changes to trigger rebuilds.

--- templates/rust-actix/src-actix/build.rs
+++ templates/rust-actix/src-actix/build.rs
@@ -1,16 +1,23 @@
-use std::{env};
-use std::process::Command;
+use npm_rs::{NodeEnv, NpmEnv};
+use std::fs;
+use walkdir::WalkDir;
 
 fn main() {
-	// Run `npm run build frontend`
-	let status = Command::new("npm")
-		.arg("run")
-		.arg("build frontend")
-		.current_dir(env::current_dir().unwrap())
-		.status()
-		.expect("Failed to execute npm 'build frontend'");
-
-	if !status.success() {
-		panic!("npm 'build frontend' failed!");
+	for entry in WalkDir::new("src") {
+		let entry = entry.unwrap();
+		if entry.file_type().is_file() {
+			println!("cargo:rerun-if-changed={}", entry.path().display());
+		}
 	}
+	fs::create_dir_all("target/dev-env").expect("failed to create target directory");
+	fs::create_dir_all("target/wwwroot").expect("failed to create wwwroot directory");
+	println!("Building frontend...");
+	// Run `npm run build frontend`
+	NpmEnv::default()
+		.with_node_env(&NodeEnv::Production)
+		.init_env()
+		.run("build-frontend")
+		.install(None)
+		.exec()
+		.expect("Failed to execute npm run 'build frontend'");
 }
